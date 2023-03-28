4e42510ae39fb50dad0ff93f8d40d543f94b572d
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-03-28 01:01:36 -0400

    Add PowerShell hugo audit and other checks
    Adds basic versions of the hugo-audit and related site validation
    scripts for Windows, using PowerShell.

---
+++ tests/scripts/check-internal-links.ps1
@@ -0,0 +1,1 @@
+npx hyperlink exampleSite\public\index.html --canonicalRoot https://example.org --internal --todo "301 http" --todo "fragment-redirect" -c 10 -r -p exampleSite\public\index.html | npx.cmd tap-spot
