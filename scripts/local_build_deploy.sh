06fc67df134ae82e0c5db605cebd7c4eefd0741e
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-08-17 23:37:42 -0400

    Update deploy to use site-specific user for SFTP
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/local_build_deploy.sh
+++ scripts/local_build_deploy.sh
@@ -5,9 +5,9 @@ set -o pipefail
 
 rm -rf public
 
-URL="https://www.hugo-theme-zen-wtg.wtg-demos.ca/"
+URL="https://www.zen-theme-hugo-wtg.wtg-demos.ca/"
 export HUGO_PARAMS_DEPLOYEDBASEURL="$URL"
 export BASEURL="$URL"
 
 HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $BASEURL --source exampleSite --destination $(pwd)/public
-rclone sync --progress public/ wtgdeml:hugo-theme-zen-wtg/
+rclone sync --progress public/ wtgdeml-dfdzen:./
